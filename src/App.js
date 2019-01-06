a157ac76508985818c007454147268463c0c9159
Damien1989 <damiengraille@gmail.com>
2019-01-06 12:30:52 +0000

    Search Bar + Rows with movies title
    Create Search Bar
    Create row showiing moving title

--- src/App.js
+++ src/App.js
@@ -2,6 +2,47 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    console.log("This is my initializer")
+
+  const movies = [
+    {id:0, title:"Avengers:Infinity War", overview:"As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle"},
+    {id:1, title:"The Avengers", overview:"This is my second overview"},
+  ]
+
+  this.state={rows:[
+  <p key="1"> This is my row0</p>,
+  <p key="2"> This is my row1</p>,
+  <p key="3"> This is my row2</p>,
+  ]}
+
+  var movieRows = []
+  movies.forEach((movie) => {
+    console.log(movie.title)
+    const movieRow = 
+    <table key={movie.id}>
+      <tbody>
+        <tr>
+          <td>
+            <img src=""/>
+          </td>
+          <td>
+            {movie.title}
+          </td>
+        </tr>
+
+      </tbody>
+    </table>
+  movieRows.push(movieRow)
+  })
+
+
+
+  this.state = {rows: movieRows}
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -19,6 +60,10 @@ class App extends Component {
              </tr>
            </tbody>
          </table>
+         <input className="inputBar"
+         placeholder="Enter search term"/>
+      
+      {this.state.rows}
       </div>
     );
   }
