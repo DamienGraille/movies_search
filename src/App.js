5388ab518c51680d69ea6a8048918b7d7c105da4
Damien1989 <damiengraille@gmail.com>
2019-01-06 10:33:42 +0000

    Add title bar
    Add title bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         <table className= "titleBar">
+           <tbody>
+             <tr>
+               <td>
+                 <img alt="app icon" width="50" src="icon.png"/>
+               </td>
+               <td width="8">
+               </td>
+               <td>
+                 <h1>MoviesDB Search</h1>
+               </td>
+             </tr>
+           </tbody>
+         </table>
       </div>
     );
   }
